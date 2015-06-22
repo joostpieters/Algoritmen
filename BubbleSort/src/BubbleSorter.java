59c2de79ce2f9fa199eb7203ed001b36b92cae8a
Slegers <yaniceslegers@gmail.com>
2015-06-22 22:09:21 +0200

    Bubble sort impl, not sure is efficient

--- BubbleSort/src/BubbleSorter.java
+++ BubbleSort/src/BubbleSorter.java
@@ -1,16 +1,18 @@
+
 /**
- * A class that sorts an given array with the bubble sort algorithm. The class also provides
- * a method to return the number of compares and swaps this algorithm makes. 
- *  
+ * A class that sorts an given array with the bubble sort algorithm. The class
+ * also provides a method to return the number of compares and swaps this
+ * algorithm makes.
+ * 
  * @author Yanice Slegers
  *
- *	TODO: make abstract class
+ *         TODO: make abstract class
  *
  */
 public class BubbleSorter {
 
 	/**
-	 * Variable that contains the array that needs to be sorted. 
+	 * Variable that contains the array that needs to be sorted.
 	 */
 	@SuppressWarnings("rawtypes")
 	private Comparable[] row;
@@ -22,56 +24,63 @@ public class BubbleSorter {
 	 * Variable containing the number of swaps this algorithm has made.
 	 */
 	private int swaps;
+
 	/**
 	 * Initializes this class with an array.
 	 * 
-	 * @param	row
-	 * 			The row that needs to be sorted.
+	 * @param row
+	 *            The row that needs to be sorted.
 	 */
 	@SuppressWarnings("rawtypes")
-	public BubbleSorter(Comparable[] row){
+	public BubbleSorter(Comparable[] row) {
 		this.row = row;
 	}
-	
+
 	/**
 	 * Returns the number of compares this algorithm makes.
 	 */
-	public int getCompares(){
+	public int getCompares() {
 		return compares;
 	}
+
 	/**
 	 * Increases the number of compares this algorithm has made by one.
 	 */
-	public void increaseCompares(){
-			compares++;
+	public void increaseCompares() {
+		compares++;
 	}
+
 	/**
 	 * Return the number of swaps this arrays has made.
 	 */
-	public int getSwaps(){
+	public int getSwaps() {
 		return swaps;
 	}
+
 	/**
 	 * Increases the number of compares this algorithm has made by one.
 	 */
-	public void increaseSwaps(){
+	public void increaseSwaps() {
 		swaps++;
 	}
-	
+
 	/**
-	 * Checks if the element of the first index is less than the element of the second index.
-	 * @param	i
-	 * 			The first index of this row that is compared.
-	 * @param	j
-	 * 			The second index of this row that is compared.
-	 * @return	True if and only if the element if the first index is less than the element of the second index.
+	 * Checks if the element of the first index is less than the element of the
+	 * second index.
+	 * 
+	 * @param 	i
+	 *        	The first index of this row that is compared.
+	 * @param 	j
+	 *        	The second index of this row that is compared.
+	 * @return 	True if and only if the element if the first index is less than
+	 *         	the element of the second index.
 	 * 
 	 */
 	@SuppressWarnings("unchecked")
-	public boolean isLess(int i,int j){
-		return row[i].compareTo(row) < 0;
+	public boolean isLess(int i, int j) {
+		return row[i].compareTo(row[j]) < 0;
 	}
-	
+
 	/**
 	 * Prints the array.
 	 */
@@ -85,10 +94,10 @@ public class BubbleSorter {
 	/**
 	 * Switches two elements of the array that needs to be sorted.
 	 * 
-	 * @param 	a
-	 *        	The first index that needs to be switched.
-	 * @param 	b
-	 *          The second index that needs to be switched.
+	 * @param a
+	 *            The first index that needs to be switched.
+	 * @param b
+	 *            The second index that needs to be switched.
 	 */
 	public void switchPlace(int a, int b) {
 		@SuppressWarnings("rawtypes")
@@ -96,8 +105,25 @@ public class BubbleSorter {
 		row[a] = row[b];
 		row[b] = temp;
 	}
-	
-	public void sort(){
-		
+
+	public void sort() {
+		for(int j = row.length; j > 0; j--){
+			for(int i = 0; i < j-1; i++){
+				showArray();
+				increaseCompares();
+				if(!isLess(i, i+1)){
+					switchPlace(i, i+1);
+				}
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		//[] e = {"E","A","S","Y","Q","U","S","T","I","O","N"};
+		Comparable[] e = {9,4,6,1,8};
+		BubbleSorter s = new BubbleSorter(e);
+		s.sort();
+		s.showArray();
 	}
 }
